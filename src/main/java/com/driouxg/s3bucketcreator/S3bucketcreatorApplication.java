7118cd2c65a8461f35c58b4da0c6ccefd549ea86
Drioux Guidry <drioux_guidry@choicehotels.com>
2020-11-30 18:56:37 -0700

    Initial commit

---
+++ src/main/java/com/driouxg/s3bucketcreator/S3bucketcreatorApplication.java
@@ -0,0 +1,13 @@
+package com.driouxg.s3bucketcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3bucketcreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3bucketcreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driouxg/s3bucketcreator/S3bucketcreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driouxg.s3bucketcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3bucketcreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
